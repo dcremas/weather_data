f3cdb95b79bd2c49a2785b423c3348bd863b7079
Dustin Cremascoli <dustincremascoli@aol.com>
2023-09-21 16:45:19 -0500

    Added logic to the csv pulls from the NOAA repo.

--- current_year_generate.py
+++ current_year_generate.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from datetime import datetime
 
 select_cols = [
     'STATION', 'DATE', 'SOURCE', 'REPORT_TYPE',
@@ -12,6 +13,8 @@ files = [file.split('.')[0] for file in os.listdir('yearly_files/2023') if file.
 
 for current_id in files:
     file_ftp = path + current_id + '.csv'
-    df = pd.read_csv(file_ftp, usecols=select_cols, low_memory=False)
     file_local = 'yearly_files/2023/' + current_id + '.csv'
+    if datetime.fromtimestamp(os.path.getctime(file_local)).day == datetime.now().day:
+        continue
+    df = pd.read_csv(file_ftp, usecols=select_cols, low_memory=False)
     df.to_csv(file_local)
